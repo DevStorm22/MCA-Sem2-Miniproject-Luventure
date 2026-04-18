d33a275742d61b7b3fd4e4b367a0fe3577e0b119
Kaushik <kaushikboss0@gmail.com>
2026-04-18 13:24:51 +0530

    feat: add mongodb atlas connection layer using mongoose

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,8 @@
 import app from "./app";
+import { connectDB } from "./config/db";
 import { env } from "./config/env";
 
-app.listen(env.port, () => {
+app.listen(env.port, async () => {
+    await connectDB();
     console.log(`Server running on port ${env.port}`);
 });
